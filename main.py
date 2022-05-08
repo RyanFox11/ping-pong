6aceaa4639556c77616b4af8e83cb1bf98767654
Ryan_Fox <rn-fox@yandex.ru>
2022-05-08 14:48:53 +0300

    added main filles

---
+++ main.py
@@ -0,0 +1,26 @@
+# OFNJ IVNOMDV IJDOKL VFV OFLV FNVOFNGJTNGIUNGTOGNTIUGN
+from pygame import *
+win_width = 1200
+win_height = 500
+display.set_caption("Ping-Pong")
+window = display.set_mode((win_width, win_height))
+window.fill((0, 0, 0))
+
+class GameSprite(sprite.Sprite):
+ # конструктор класса
+   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+       # Вызываем конструктор класса (Sprite):
+       sprite.Sprite.__init__(self)
+ 
+       # каждый спрайт должен хранить свойство image - изображение
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.speed = player_speed
+ 
+       # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+ # метод, отрисовывающий героя на окне
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
